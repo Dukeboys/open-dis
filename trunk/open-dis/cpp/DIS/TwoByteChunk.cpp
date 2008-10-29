277d56d8a7a22d8faef961fe829c85b835f37e47
mcgredo <mcgredo@1da32bc5-2e4a-4c7f-92e1-9780f3f753aa>
2008-10-29 00:14:47 +0000

    Initialize fixed length array in ctor
    git-svn-id: svn://svn.code.sf.net/p/open-dis/code@142 1da32bc5-2e4a-4c7f-92e1-9780f3f753aa

--- trunk/open-dis/cpp/DIS/TwoByteChunk.cpp
+++ trunk/open-dis/cpp/DIS/TwoByteChunk.cpp
@@ -5,6 +5,13 @@ using namespace DIS;
 
 TwoByteChunk::TwoByteChunk()
 {
+     // Initialize fixed length array
+     int lengthotherParameters = 0
+     for(lengthotherParameters= 0; lengthotherParameters < 2; lengthotherParameters++)
+     {
+         otherParameters[lengthotherParameters] = 0
+     }
+
 }
 
 TwoByteChunk::~TwoByteChunk()
