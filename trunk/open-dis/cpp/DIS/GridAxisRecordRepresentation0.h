ac73a6875b5c0a12c5fd9b3904b059f54a3e4bbf
mcgredo <mcgredo@1da32bc5-2e4a-4c7f-92e1-9780f3f753aa>
2009-06-29 20:44:17 +0000

    Irrelevant change to comments, avoiding use of @ character
    git-svn-id: svn://svn.code.sf.net/p/open-dis/code@415 1da32bc5-2e4a-4c7f-92e1-9780f3f753aa

--- trunk/open-dis/cpp/DIS/GridAxisRecordRepresentation0.h
+++ trunk/open-dis/cpp/DIS/GridAxisRecordRepresentation0.h
@@ -22,7 +22,7 @@ protected:
   /** number of bytes of environmental state data */
   unsigned short _numberOfBytes; 
 
-  /** variable length list of data parameters @@@this is wrong--need padding as well */
+  /** variable length list of data parameters ^^^this is wrong--need padding as well */
   std::vector<OneByteChunk> _dataValues; 
 
 
