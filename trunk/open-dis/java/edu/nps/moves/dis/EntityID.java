3907912d28b4ab547a1b2fdc2a4fb16007b0dda9
mcgredo <mcgredo@1da32bc5-2e4a-4c7f-92e1-9780f3f753aa>
2009-07-11 06:43:17 +0000

    Idiociy
    git-svn-id: svn://svn.code.sf.net/p/open-dis/code@447 1da32bc5-2e4a-4c7f-92e1-9780f3f753aa

--- trunk/open-dis/java/edu/nps/moves/dis/EntityID.java
+++ trunk/open-dis/java/edu/nps/moves/dis/EntityID.java
@@ -145,20 +145,6 @@ public void unmarshal(java.nio.ByteBuffer buff)
     return ivarsEqual;
  }
 
-/**
- * Override of base class hashcode. This is convienent to use when looking up entities in
- * a hash table. It uses the application and entity IDs to fill out the 32 bits. The idea
- * is to get a "pretty close" to unique hashcode for each entity ID in 32 bits, and the
- * best way to do that is leave out the site ID while keeping the rest, which tends to
- * be more unique.
- */
-public int hashcode()
-{
-	int hashcode = site;
-	hashcode = hashcode << 16;
-	hashcode = hashcode + entity;
-	return hashcode;
-}
-	
+
 	
 } // end of class
