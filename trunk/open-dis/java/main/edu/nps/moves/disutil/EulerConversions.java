017a9d80eb9ada35e3b92e3c74bfd7b247e46d7f
mcgredo <mcgredo@1da32bc5-2e4a-4c7f-92e1-9780f3f753aa>
2010-05-28 23:15:11 +0000

    new code for getting pitch, roll from euler angles
    git-svn-id: svn://svn.code.sf.net/p/open-dis/code@534 1da32bc5-2e4a-4c7f-92e1-9780f3f753aa

--- trunk/open-dis/java/main/edu/nps/moves/disutil/EulerConversions.java
+++ trunk/open-dis/java/main/edu/nps/moves/disutil/EulerConversions.java
@@ -1,10 +1,12 @@
 package edu.nps.moves.disutil;
 
 /**
- * Converts a given pose--latitude, longitude, and the euler angles psi and theta--
- * into a degree heading<p>
+ * Converts into Tait_Bryan_angles of roll, pitch and yaw/heading given the
+ * position--latitude, longitude, and the euler angles psi, theta, and phi.
+ * 
  *
- * @author loyaj
+ * @author loyaj & bhughes
+ *
  */
 
 public class EulerConversions
@@ -40,5 +42,70 @@ public class EulerConversions
         double b12 = -sincos * cosThetaCosPsi - sinsin * cosThetaSinPsi - coslat * sinTheta;
 
         return Math.toDegrees(Math.atan2(b11, b12));//range is -pi to pi
+    }
+
+    /**
+     * Gets a degree pitch for an entity based on euler angles. All angular values passed in must be in radians.
+     * @param lat Entity's latitude, IN RADIANS
+     * @param lon Entity's longitude, IN RADIANS
+     * @param psi Psi angle
+     * @param theta Theta angle
+     * @return the pitch, in degrees, with 0 being level.A negative values is the entity's
+     * nose is pointing downward, positive when the entity's nose is pointing upward.
+     */
+    public static double getPitchFromEuler(double lat, double lon, double psi, double theta)
+    {
+        double sinlat = Math.sin(lat);
+        double sinlon = Math.sin(lon);
+        double coslon = Math.cos(lon);
+        double coslat = Math.cos(lat);
+        double cosLatCosLon = coslat * coslon;
+        double cosLatSinLon = coslat * sinlon;
+
+        double cosTheta = Math.cos(theta);
+        double cosPsi = Math.cos(psi);
+        double sinPsi = Math.sin(psi);
+        double sinTheta = Math.sin(theta);
+
+        return Math.toDegrees(Math.asin(cosLatCosLon*cosTheta*cosPsi + cosLatSinLon*cosTheta*sinPsi - sinlat*sinTheta));
+    }
+
+    /**
+     * Gets the degree roll for an entity based on euler angles. All angular values passed in must be in radians.
+     * @param lat Entity's latitude, IN RADIANS
+     * @param lon Entity's longitude, IN RADIANS
+     * @param psi Psi angle
+     * @param theta Theta angle
+     * @param phi Phi angle
+     * @return the roll, in degrees, with 0 being level flight.
+     */
+    public static double getRollFromEuler(double lat, double lon, double psi, double theta, double phi)
+    {
+        double sinlat = Math.sin(lat);
+        double sinlon = Math.sin(lon);
+        double coslon = Math.cos(lon);
+        double coslat = Math.cos(lat);
+        double cosLatCosLon = coslat * coslon;
+        double cosLatSinLon = coslat * sinlon;
+
+        double cosTheta = Math.cos(theta);
+        double cosPsi   = Math.cos(psi);
+        double sinPsi   = Math.sin(psi);
+        double sinTheta = Math.sin(theta);
+        double sinPhi   = Math.sin(phi);
+        double cosPhi   = Math.cos(phi);
+
+        double sinPhiSinTheta = sinPhi * sinTheta;
+        double cosPhiSinTheta = cosPhi * sinTheta;
+
+        double b23 = cosLatCosLon*(-cosPhi*sinPsi + sinPhiSinTheta*cosPsi) +
+                     cosLatSinLon*( cosPhi*cosPsi + sinPhiSinTheta*sinPsi) +
+                     sinlat * (sinPhi * cosTheta);
+
+        double b33 = cosLatCosLon*( sinPhi*sinPsi + cosPhiSinTheta*cosPsi) +
+                     cosLatSinLon*(-sinPhi*cosPsi + cosPhiSinTheta*sinPsi) +
+                     sinlat * (cosPhi * cosTheta);
+
+        return Math.toDegrees(Math.atan2(-b23, -b33));
     }
 }
