c9cae1c8f3a104a70ea04b60eeaefe7a9b29bebb
mcgredo <mcgredo@1da32bc5-2e4a-4c7f-92e1-9780f3f753aa>
2010-06-04 19:22:05 +0000

    Updated for reverse operations on euler conversions
    git-svn-id: svn://svn.code.sf.net/p/open-dis/code@540 1da32bc5-2e4a-4c7f-92e1-9780f3f753aa

--- trunk/open-dis/java/main/edu/nps/moves/disutil/EulerConversions.java
+++ trunk/open-dis/java/main/edu/nps/moves/disutil/EulerConversions.java
@@ -1,8 +1,17 @@
 package edu.nps.moves.disutil;
 
 /**
- * Converts into Tait_Bryan_angles of roll, pitch and yaw/heading given the
- * position--latitude, longitude, and the euler angles psi, theta, and phi.
+ * Class contains methods that convert to Tait_Bryan_angles (i.e., roll, pitch
+ * and yaw/heading) given the position (i.e., latitude, longitude) and the
+ * euler angles (i.e., psi, theta, and phi).
+ *
+ * Class also has methods for the corollary: converting to psi, theta, and phi
+ * given the lat/lon position and the entity's roll, pitch and yaw angles
+ *
+ * In this class roll, pitch and yaw are always expressed in degrees
+ * whereas psi, theta, and phi are always in radians.
+ *
+ * Note: latitude and longitude are also expressed in radians.
  * 
  *
  * @author loyaj & bhughes
@@ -11,14 +20,18 @@ package edu.nps.moves.disutil;
 
 public class EulerConversions
 {
+
+    static double _toDegrees = 57.2957795131;
+    static double _toRadians =  0.01745329252;
+
     /**
      * Gets a degree heading for an entity based on euler angles. All angular values passed in must be in radians.
-     * @param lat Entity's latitude, IN RADIANS
-     * @param lon Entity's longitude, IN RADIANS
-     * @param psi Psi angle
-     * @param theta Theta angle
+     * @param lat Entity's latitude,    IN RADIANS
+     * @param lon Entity's longitude,   IN RADIANS
+     * @param psi Psi angle,            IN RADIANS
+     * @param theta Theta angle,        IN RADIANS
      * @return the heading, in degrees, with 0 being north, positive angles going clockwise,
-     * and negative angles going counterclockwise
+     * and negative angles going counterclockwise (i.e., 90 deg is east, -90 is west)
      */
     public static double getOrientationFromEuler(double lat, double lon, double psi, double theta)
     {
@@ -46,12 +59,12 @@ public class EulerConversions
 
     /**
      * Gets a degree pitch for an entity based on euler angles. All angular values passed in must be in radians.
-     * @param lat Entity's latitude, IN RADIANS
-     * @param lon Entity's longitude, IN RADIANS
-     * @param psi Psi angle
-     * @param theta Theta angle
-     * @return the pitch, in degrees, with 0 being level.A negative values is the entity's
-     * nose is pointing downward, positive when the entity's nose is pointing upward.
+     * @param lat Entity's latitude,    IN RADIANS
+     * @param lon Entity's longitude,   IN RADIANS
+     * @param psi Psi angle,            IN RADIANS
+     * @param theta Theta angle,        IN RADIANS
+     * @return the pitch, in degrees, with 0 being level. A negative values is when the entity's
+     * nose is pointing downward, positive value is when the entity's nose is pointing upward.
      */
     public static double getPitchFromEuler(double lat, double lon, double psi, double theta)
     {
@@ -72,12 +85,12 @@ public class EulerConversions
 
     /**
      * Gets the degree roll for an entity based on euler angles. All angular values passed in must be in radians.
-     * @param lat Entity's latitude, IN RADIANS
-     * @param lon Entity's longitude, IN RADIANS
-     * @param psi Psi angle
-     * @param theta Theta angle
-     * @param phi Phi angle
-     * @return the roll, in degrees, with 0 being level flight.
+     * @param lat Entity's latitude,    IN RADIANS
+     * @param lon Entity's longitude,   IN RADIANS
+     * @param psi Psi angle,            IN RADIANS
+     * @param theta Theta angle,        IN RADIANS
+     * @param phi Phi angle,            IN RADIANS
+     * @return the roll, in degrees, with 0 being level flight, + roll is clockwise when looking out the front of the entity.
      */
     public static double getRollFromEuler(double lat, double lon, double psi, double theta, double phi)
     {
@@ -89,9 +102,9 @@ public class EulerConversions
         double cosLatSinLon = coslat * sinlon;
 
         double cosTheta = Math.cos(theta);
+        double sinTheta = Math.sin(theta);
         double cosPsi   = Math.cos(psi);
         double sinPsi   = Math.sin(psi);
-        double sinTheta = Math.sin(theta);
         double sinPhi   = Math.sin(phi);
         double cosPhi   = Math.cos(phi);
 
@@ -108,4 +121,82 @@ public class EulerConversions
 
         return Math.toDegrees(Math.atan2(-b23, -b33));
     }
+
+    /**
+     * Gets the Euler Theta value (in radians) from position and Tait-Brayn yaw and roll angles
+     * @param lat Entity's latitude,    IN RADIANS
+     * @param lon Entity's longitude,   IN RADIANS
+     * @param yaw   entity's yaw angle (also know as the entity's bearing or heading angle), in degrees
+     * @param pitch entity's pitch angle, in degrees
+     * @return the Theta value in radians
+     */
+    public static double getThetaFromTaitBryanAngles(double lat, double lon, double yaw, double pitch)
+    {
+        double sinLat = Math.sin(lat);
+        double cosLat = Math.cos(lat);
+
+        double cosPitch = Math.cos(pitch*_toRadians);
+        double sinPitch = Math.sin(pitch*_toRadians);
+        double cosYaw   = Math.cos(yaw*_toRadians);
+
+        return Math.asin(  -cosLat * cosYaw * cosPitch - sinLat * sinPitch );
+    }
+
+    /**
+     * Gets the Euler Psi value (in radians) from position and Tait-Brayn yaw and roll angles
+     * @param lat Entity's latitude,    IN RADIANS
+     * @param lon Entity's longitude,   IN RADIANS
+     * @param yaw   ettity's yaw angle (also know as the entity's bearing or heading angle), in degrees
+     * @param pitch entity's pitch angle, in degrees
+     * @return the Psi value in radians
+     */
+    public static double getPsiFromTaitBryanAngles(double lat, double lon, double yaw, double pitch){
+
+        double sinLat = Math.sin(lat);
+        double sinLon = Math.sin(lon);
+        double cosLon = Math.cos(lon);
+        double cosLat = Math.cos(lat);
+        double cosLatCosLon = cosLat * cosLon;
+        double cosLatSinLon = cosLat * sinLon;
+        double sinLatCosLon = sinLat * cosLon;
+        double sinLatSinLon = sinLat * sinLon;
+
+        double cosPitch = Math.cos(pitch*_toRadians);
+        double sinPitch = Math.sin(pitch*_toRadians);
+        double sinYaw   = Math.sin(yaw*_toRadians);
+        double cosYaw   = Math.cos(yaw*_toRadians);
+
+        double a_11 = -sinLon * sinYaw * cosPitch - sinLatCosLon * cosYaw * cosPitch + cosLatCosLon * sinPitch;
+        double a_12 =  cosLon * sinYaw * cosPitch - sinLatSinLon * cosYaw * cosPitch + cosLatSinLon * sinPitch;
+
+        return Math.atan2(a_12, a_11);
+    }
+
+    /**
+     * Gets the Euler Phi value (in radians) from position and Tait-Brayn yaw, pitch and roll angles
+     * @param lat Entity's latitude,    IN RADIANS
+     * @param lon Entity's longitude,   IN RADIANS
+     * @param yaw yaw angle (also know as the entity's bearing or heading angle), in degrees
+     * @param pitch entity's pitch angle, in degrees
+     * @param roll  entity's roll angle (0 is level flight, + roll is clockwise looking out the nose), in degrees
+     * @return the Phi value in radians
+     */
+    public static double getPhiFromTaitBryanAngles(double lat, double lon, double yaw, double pitch, double roll){
+
+        double sinLat = Math.sin(lat);
+        double cosLat = Math.cos(lat);
+
+        double cosRoll  = Math.cos(roll*_toRadians);
+        double sinRoll  = Math.sin(roll*_toRadians);
+        double cosPitch = Math.cos(pitch*_toRadians);
+        double sinPitch = Math.sin(pitch*_toRadians);
+        double sinYaw   = Math.sin(yaw*_toRadians);
+        double cosYaw   = Math.cos(yaw*_toRadians);
+
+        double a_23 = cosLat * (-sinYaw * cosRoll + cosYaw * sinPitch * sinRoll) - sinLat * cosPitch * sinRoll;
+        double a_33 = cosLat * ( sinYaw * sinRoll + cosYaw * sinPitch * cosRoll) - sinLat * cosPitch * cosRoll;
+
+        return Math.atan2(a_23, a_33);
+    }
+
 }
