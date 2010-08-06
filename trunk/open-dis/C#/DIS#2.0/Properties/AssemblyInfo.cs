9210d50244bfada412ceab962fa8a28cdc41213e
pesmith <pesmith@1da32bc5-2e4a-4c7f-92e1-9780f3f753aa>
2010-08-06 22:24:37 +0000

    Project file provided by Zvonko
    git-svn-id: svn://svn.code.sf.net/p/open-dis/code@554 1da32bc5-2e4a-4c7f-92e1-9780f3f753aa

---
+++ trunk/open-dis/C#/DIS#2.0/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Open DIS Library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Blubit d.o.o.")]
+[assembly: AssemblyProduct("Open DIS, http://open-dis.sourceforge.org")]
+[assembly: AssemblyCopyright("Copyright © Blubit d.o.o. 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("719719e6-e1a9-4428-aa7a-e8d2defd1b75")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
