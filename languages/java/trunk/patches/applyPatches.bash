aaf77957948fc77ee9532155028d85ccc4062b7b
mcgredo <mcgredo@1da32bc5-2e4a-4c7f-92e1-9780f3f753aa>
2010-12-09 22:23:27 +0000

    Modified patch script to take a "product" arg that defines which package is being patched, separate directories for patches for each package
    git-svn-id: svn://svn.code.sf.net/p/open-dis/code@591 1da32bc5-2e4a-4c7f-92e1-9780f3f753aa

--- languages/java/trunk/patches/applyPatches.bash
+++ languages/java/trunk/patches/applyPatches.bash
@@ -10,6 +10,9 @@
 #
 # Uses the Unix patch utility.
 #
+# Arguments: $0 = shell script name (this file)
+# $1 = product: dis, dismobile, etc. 
+#
 # To generate a patch file, check out the current source code from svn, then
 # modify it. Run the command
 # svn diff SomeFile.java > SomeFile.java.patch
@@ -18,20 +21,47 @@
 # @author DMcG
 # @version $Id:$
 
-# Pdu.java, apply patch file
-patch src/main/edu/nps/moves/dis/Pdu.java patches/java/Pdu.patch
+echo Arguments: $0, $1, $2
 
-# Marking.java, apply patch file to ensure character fields are exactly 11 bytes long after set operations
-patch src/main/edu/nps/moves/dis/Marking.java patches/java/Marking.patch
+case "$1" in
+  dis)
+      # Pdu.java, apply patch file
+      patch src/main/edu/nps/moves/dis/Pdu.java patches/dis/Pdu.patch
 
-# Apply patch to VariableDatum.java
-patch src/main/edu/nps/moves/dis/VariableDatum.java patches/java/VariableDatum.patch
+      # Marking.java, apply patch file to ensure character fields are exactly 11 bytes long after set operations
+      patch src/main/edu/nps/moves/dis/Marking.java patches/dis/Marking.patch
 
-# Apply patch to EntityID to get equality, hashcode
-patch src/main/edu/nps/moves/dis/EntityID.java patches/java/EntityID.patch
+      # Apply patch to VariableDatum.java
+      patch src/main/edu/nps/moves/dis/VariableDatum.java patches/dis/VariableDatum.patch
 
-# Apply patch to Vector3Double to get some convienience methods for 
-# converting back and forth to DIS coordinates from lat/lon/alt.
-patch src/main/edu/nps/moves/dis/Vector3Double.java patches/java/Vector3Double.patch
+      # Apply patch to EntityID to get equality, hashcode
+      patch src/main/edu/nps/moves/dis/EntityID.java patches/dis/EntityID.patch
 
+      # Apply patch to Vector3Double to get some convienience methods for 
+      # converting back and forth to DIS coordinates from lat/lon/alt.
+      patch src/main/edu/nps/moves/dis/Vector3Double.java patches/dis/Vector3Double.patch
+      ;;
 
+  dismobile)
+     # Pdu.java, apply patch file
+      patch src/main/edu/nps/moves/dismobile/Pdu.java patches/dismobile/Pdu.patch
+
+      # Marking.java, apply patch file to ensure character fields are exactly 11 bytes long after set operations
+      patch src/main/edu/nps/moves/dismobile/Marking.java patches/dismobile/Marking.patch
+
+      # Apply patch to VariableDatum.java
+      patch src/main/edu/nps/moves/dismobile/VariableDatum.java patches/dismobile/VariableDatum.patch
+
+      # Apply patch to EntityID to get equality, hashcode
+      patch src/main/edu/nps/moves/dismobile/EntityID.java patches/dismobile/EntityID.patch
+
+      # Apply patch to Vector3Double to get some convienience methods for
+      # converting back and forth to DIS coordinates from lat/lon/alt.
+      patch src/main/edu/nps/moves/dismobile/Vector3Double.java patches/dismobile/Vector3Double.patch
+      ;;
+ 
+   *)
+     echo product not found. You should set the "product" ant property in the build.xml file.
+     exit
+     ;;
+ esac     
