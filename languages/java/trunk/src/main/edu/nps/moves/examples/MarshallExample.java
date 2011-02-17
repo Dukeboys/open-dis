710c9e21311ede549ceb5c8a35d2a966a4be93cf
mcgredo <mcgredo@1da32bc5-2e4a-4c7f-92e1-9780f3f753aa>
2011-02-17 01:02:57 +0000

    Read back out and print class types from XML file
    git-svn-id: svn://svn.code.sf.net/p/open-dis/code@638 1da32bc5-2e4a-4c7f-92e1-9780f3f753aa

--- languages/java/trunk/src/main/edu/nps/moves/examples/MarshallExample.java
+++ languages/java/trunk/src/main/edu/nps/moves/examples/MarshallExample.java
@@ -4,6 +4,7 @@ import edu.nps.moves.dis.*;
 
 import java.io.*;
 import javax.xml.bind.*;
+import java.util.*;
 
 /**
  * Example of using JAXB to marshal and unmarshal to XML. Uses
@@ -38,7 +39,9 @@ public class MarshallExample {
         container.getPdus().add(detonationPdu);
         container.getPdus().add(bang);
         
-        for(int idx = 0; idx < 500; idx++)
+		// Create some PDUs, setting the location, velocity, and entity IDs
+		// to be random numbers.
+        for(int idx = 0; idx < 20; idx++)
         {
             EntityStatePdu pdu = new EntityStatePdu();
             
@@ -76,6 +79,17 @@ public class MarshallExample {
             Unmarshaller unmarshaller = context.createUnmarshaller();
             PduContainer unmarshalledObject = (PduContainer) unmarshaller.unmarshal(new FileInputStream("somePdus.xml"));
             System.out.println("got " + unmarshalledObject.getNumberOfPdus() + " back");
+			
+			List<Pdu> unmarshalledPdus = unmarshalledObject.getPdus();
+			
+			System.out.println("Marshalled PDUs out to XML, read them back, contents of list are...");
+			for(int idx = 0; idx < unmarshalledPdus.size(); idx++)
+			{
+				Pdu aPdu = unmarshalledPdus.get(idx);
+				System.out.println("   Pdu Type: " + aPdu.getClass().getName());
+			}
+			
+			
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println(e);
