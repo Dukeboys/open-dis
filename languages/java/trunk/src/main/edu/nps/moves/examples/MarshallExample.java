12a21fd26665c1987c1baa39bad7f8df4788c0b7
mcgredo <mcgredo@1da32bc5-2e4a-4c7f-92e1-9780f3f753aa>
2011-04-12 17:10:30 +0000

    Added some comments
    git-svn-id: svn://svn.code.sf.net/p/open-dis/code@645 1da32bc5-2e4a-4c7f-92e1-9780f3f753aa

--- languages/java/trunk/src/main/edu/nps/moves/examples/MarshallExample.java
+++ languages/java/trunk/src/main/edu/nps/moves/examples/MarshallExample.java
@@ -7,7 +7,7 @@ import javax.xml.bind.*;
 import java.util.*;
 
 /**
- * Example of using JAXB to marshal and unmarshal to XML. Uses
+ * Example of using JAXB to marshal and unmarshall PDUs to and from XML. Uses
  * Jaxb 2.1.x.
  * 
  * @author DMcG
@@ -21,9 +21,13 @@ public class MarshallExample {
      */
     public static void main(String args[]) {
 
+        // PduContainer is simply a list of PDUs. The PDUs can be of any type;
+        // espdu, fire, detonation, etc. The PDU container is an XML root element,
+        // meaning that it can be a top level XML document.
+        
         PduContainer container = new PduContainer(); // XmlRootElement marked class
 
-        // A few PDUs to marshal
+        // Create a few PDUs to marshal
         EntityStatePdu espdu = new EntityStatePdu();
         FirePdu firePdu = new FirePdu();
         DetonationPdu detonationPdu = new DetonationPdu();
@@ -35,8 +39,9 @@ public class MarshallExample {
         container.getPdus().add(detonationPdu);
         container.getPdus().add(bang);
         
-		// Create some more PDUs, setting the location, velocity, and entity IDs
-		// to be random numbers.
+	// Create some more espdus, setting the location, velocity, and entity IDs
+	// to be random numbers. This will show that the PDU attributes are actually
+        // being marshalled.
         for(int idx = 0; idx < 20; idx++)
         {
             EntityStatePdu pdu = new EntityStatePdu();
@@ -60,45 +65,61 @@ public class MarshallExample {
         }
 
         // And marshal it out to an XML file. We have the PduContainer, which 
-		// holds all the PDUs. Marshal it out to the given XML file.
+        // holds all the PDUs. Marshal it out to the given XML file. This uses
+        // jaxb; see the javadoc at the JDK 1.6 documents, in the javax.xml.bind
+        // package.
+
         try 
-		{
-			// The context, which will contain the package of the classes we marshal
-			// out
+        {
+            // The context, which will contain the package of the classes we marshal out
             JAXBContext context = JAXBContext.newInstance("edu.nps.moves.dis"); 
 			
-			// Choose the file to send to, and make it pretty-print
+            // Choose the file to send to, and make it pretty-print. The marshaller
+            // object is responsible for writing the XML, but it needs to be told
+            // a few things, such as the file to write to, the format to write it
+            // in, and so on. This is part of JAXB;
+
             Marshaller marshaller = context.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             marshaller.marshal(container, new FileOutputStream("somePdus.xml"));
             
 
             // Just to prove nothing funny is going on, read the XML file back in
+            // from the file we just wrote to. Again, this primarily relies on JAXB,
+            // from the javax.xml.bind package for Unmarshaller, JaxbContext, and so on.
+
             Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            // We get back a PduContainer object from the XML file--this is a container
+            // the holds zero or more PDUs.
+
             PduContainer unmarshalledObject = (PduContainer) unmarshaller.unmarshal(new FileInputStream("somePdus.xml"));
             System.out.println("got " + unmarshalledObject.getNumberOfPdus() + " back");
+
+            // Print out the PDUs; retreive the list of PDUs from the PDU Container
+            List<Pdu> unmarshalledPdus = unmarshalledObject.getPdus();
 			
-			List<Pdu> unmarshalledPdus = unmarshalledObject.getPdus();
-			
-			System.out.println("Marshalled PDUs out to XML, read them back, contents of list are...");
-			for(int idx = 0; idx < unmarshalledPdus.size(); idx++)
-			{
-				Pdu aPdu = unmarshalledPdus.get(idx);
-				System.out.println("   Pdu Type: " + aPdu.getClass().getName());
-				if(aPdu instanceof EntityStatePdu)
-				{
-					EntityStatePdu es = (EntityStatePdu)aPdu;
-					System.out.println("      EID:(" + es.getEntityID().getSite() + "," + 
-									           es.getEntityID().getApplication() + "," + 
-									           es.getEntityID().getEntity() + ")");
-				}
-			}
-			
-			
-        } catch (Exception e) {
+            System.out.println("Marshalled PDUs out to XML, read them back, contents of list are...");
+            for(int idx = 0; idx < unmarshalledPdus.size(); idx++)
+            {
+                    Pdu aPdu = unmarshalledPdus.get(idx);
+                    System.out.println("   Pdu Type: " + aPdu.getClass().getName());
+
+                    // For the entity state PDUs we put into the PDU container, print out
+                    // the contents for the attributes we set
+                    if(aPdu instanceof EntityStatePdu)
+                    {
+                         EntityStatePdu es = (EntityStatePdu)aPdu;
+                         System.out.println("      EID:(" + es.getEntityID().getSite() + "," +
+                                                                       es.getEntityID().getApplication() + "," +
+                                                                       es.getEntityID().getEntity() + ")");
+                    }
+            }		
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
             System.out.println(e);
-
         }
     }
 }
